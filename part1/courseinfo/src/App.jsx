9c18425b6efe43f59f376e8f66a31d8829204520
Faisal-Mahmud-Anik <bandafaisalmahmud@gmail.com>
2023-10-31 15:18:43 +0600

    Exercise 1.3-1.5 completed

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,25 +1,28 @@
 // ------------ Parent Component
 const App = () => {
-  const course = "Half Stack application development";
-  const part1 = "Fundamental of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercise={exercises1} />
-      <Content part={part2} exercise={exercises2} />
-      <Content part={part3} exercise={exercises3} />
-
-      <Total
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
-      />
+      <Content course={course} />
+      <Total course={course} />
     </div>
   );
 };
@@ -27,26 +30,37 @@ const App = () => {
 export default App;
 
 // ---------------- Child Components
+
 const Header = ({ course }) => {
   return (
     <>
-      <h1>{course}</h1>
+      <h1>{course.name}</h1>
     </>
   );
 };
-const Content = ({ part, exercise }) => {
+
+const Content = ({ course }) => {
   return (
     <>
-      <p>
-        {part} {exercise}
-      </p>
+      {course.parts.map((part) => (
+        <p key={part.name}>
+          {part.name} {part.exercises}
+        </p>
+      ))}
+      <p></p>
     </>
   );
 };
-const Total = ({ exercises1, exercises2, exercises3 }) => {
+
+const Total = ({ course }) => {
+  const totalNumOfExercise = course.parts.reduce(
+    (acc, curr) => acc + curr.exercises,
+    0
+  );
+  console.log(totalNumOfExercise);
   return (
     <>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <p>Number of exercises {totalNumOfExercise}</p>
     </>
   );
 };
